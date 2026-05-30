c2090a9a921a2844d330d95d64a1a8715cfc2fff
DominDev <pawel.dominiak@domindev.com>
2026-05-31 01:56:42 +0200

    Configure Supabase project URL

--- frontend/js/config.js
+++ frontend/js/config.js
@@ -1,5 +1,4 @@
 export const SUPABASE_CONFIG = {
-  url: "",
-  anonKey: "",
+  url: "https://nhobczmoonkfnooclxgr.supabase.co",
+  anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5ob2Jjem1vb25rZm5vb2NseGdyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODAxNjgxNzMsImV4cCI6MjA5NTc0NDE3M30.tWmY7oD0sEqyJAw6VXzh2Ox3W_jMrrEzemJfkenO87c",
 };
-
